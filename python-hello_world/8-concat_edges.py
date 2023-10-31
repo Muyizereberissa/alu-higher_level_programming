c1f53775dbee3be7f103df0e7b7612d6d4050146
Muyizereberissa <b.muyizere@alustudent.com>
2023-10-31 00:30:59 -0700

    8-concat_edges.py

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str1 = str[39:66]
+str2 = str[107:111]
+str3 = str[0:6]
+str = str1 + " " + str2 + " " + str3
+print(str)
