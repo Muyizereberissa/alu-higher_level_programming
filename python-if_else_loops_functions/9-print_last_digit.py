4653d964f199f1e0001eb719bebd06c68b1c8836
Muyizereberissa <b.muyizere@alustudent.com>
2023-11-01 08:26:50 -0700

    uppercase.py

---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    result = ""
+    for char in str:
+        if 'a' <= char <= 'z':
+
+            char = chr(ord(char) - 32)
+        result += char
+    print(result)    
+
