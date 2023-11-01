ae2dc9f1fd5ac2a371a48e31f4cd1de0ba921dee
Muyizereberissa <b.muyizere@alustudent.com>
2023-11-01 03:27:56 -0700

    print_alphabt.py

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z'), != ord(e)&ord(q), +1):
+for i in range(ord('a'), ord('z'), +1):
+    if chr(i) != 'qe'
         print("{}".format(chr(i)), end="")
