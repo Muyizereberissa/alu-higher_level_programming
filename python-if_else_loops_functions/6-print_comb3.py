3c4e82b0ee294b6f49739c315f8144df2e938cec
Muyizereberissa <b.muyizere@alustudent.com>
2023-11-01 07:03:41 -0700

    6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for  i in range(0, 10):
-    for j in range(i +1, 10):
-        print("{:02d},".format(i * 10, j), end=" ")
-        print("{:02d}".format(9 * 10 + 10))
+for first_digit in range(10):
+    for second_digit in range(first_digit + 1, 10):
+        print("{:02d}, ".format(first_digit * 10 + second_digit), end="")
+print("{:02d}".format(9 * 10 + 9))
