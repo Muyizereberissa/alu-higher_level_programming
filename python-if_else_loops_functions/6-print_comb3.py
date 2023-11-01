af37c4ebae93f1725e014c5363e28ea7c0f67905
Muyizereberissa <b.muyizere@alustudent.com>
2023-11-01 07:18:17 -0700

    6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-for first_digit in range(10):
-    for second_digit in range(first_digit + 1, 10):
-        print("{:02d,}".format(first_digit * 10 + second_digit), end=" ")
+for i ni range(10):
+    for j in range(i, 10):
+        if i < j:
+            print("{:d}{:d}".format(i,j), end=" "
+                    if i == 8 and j == 9:
+                    else ","
