571605bb5ed4c6dcbd3766388b0e968a6daa82fb
Muyizereberissa <b.muyizere@alustudent.com>
2023-11-01 05:42:37 -0700

    print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-for  i in range(0,6):
-    for j in range (0,6):
+for  i in range(0, 6):
+    for j in range (0, 6):
         print("str(i)+str(j)".format(str), end=" "
                 if str(i) < str(j):
-                print(str(i))
-                elif str(i) > str(j):
-                print(str(j))
+                   print(str(i))
+                else:
+                   print(str(j))
