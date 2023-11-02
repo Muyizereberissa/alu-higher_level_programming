42c284643ae9e03b9950b0a7702cc99223b6570e
Muyizereberissa <b.muyizere@alustudent.com>
2023-11-02 10:45:02 -0700

    12-fizzbuzz.py

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -8,5 +8,4 @@ for num in range(1, 101):
         elif num % 5 == 0:
             print("Buzz", end=" ")
         else:
-            print(num, end=" ")
-
+            print("{:d}".format(num),  end=" ")
