0eb4b2870167f980360e8374b5ff371e16281459
Muyizereberissa <b.muyizere@alustudent.com>
2023-11-03 02:36:27 -0700

    1-calculation.py

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -4,12 +4,12 @@ if __name__ == "__main__":
 
     a = 10 
     b = 5
-    result_subtract = subtract(a, b)
+    result_subtract = subt(a, b)
     result_add = add(a, b)
-    result_multiply = multiply(a, b)
-    result_divide = divide(a, b)
+    result_multiply = mul(a, b)
+    result_divide = div(a, b)
 
     print("{:d} - {:d} = {:d}".format(a, b , sub(a, b)))
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-    print("{:d} * {:d} = {:d}".format(a, b, multiply(a, b)))
-    print("{:d} / {:d} = {:d}".format(a, b, divide(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div`(a, b)))
