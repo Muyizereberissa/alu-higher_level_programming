5901b11995219b6afd98aa571648d3416cb3de20
Muyizereberissa <b.muyizere@alustudent.com>
2023-11-03 06:09:39 -0700

    2-args.py

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ ==" __main__":
+    from sys import argv
+
+    if len(argv) == 1:
+        print("number of argument(s):.")
+    else:
+        print(f"number of arguments(s): {len(argv) - 1}{'.' if len(argv) == 2 else ':'}")
+
+        for i, arg in enumerate(argv[1:], 1):
+
+            print(f"{i}: {argv}")
