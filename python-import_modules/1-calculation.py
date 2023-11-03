0c0cd08200b2611b790219b0d146d1ecd59cd168
Muyizereberissa <b.muyizere@alustudent.com>
2023-11-03 02:13:31 -0700

    1-calculation.py

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import subtract
+    from calculator_1 import subtract, add, multiply, divide
 
     a = 10 
     b = 5
+    result_subtract = subtract(a, b)
+    result_add = add(a, b)
+    result_multiply = multiply(a, b)
+    result_divide = divide(a, b)
+
     print("{:d} - {:d} = {:d}")
+    print("{:d} + {:d} = {:d}")
+    print("{:d} * {:d} = {:d}")
+    print("{:d} / {:d} = {:d}")
