82998a8e7ddf201b2aa135418a50d79f5b262b3e
Muyizereberissa <b.muyizere@alustudent.com>
2023-11-07 04:25:29 -0800

    1-element_at.py

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    for i in idx:
-        if i < 0 and i != range(my_list):
-            print(None)
-
-        else:
-            print(my_list, idx)
+    if idx < 0 or idx >= len(my_list):
+        return None
+    else:
+        return my_list[idx]
 
