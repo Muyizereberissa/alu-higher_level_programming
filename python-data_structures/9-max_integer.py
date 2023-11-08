89cc3988497fbe01fcb8d2a4f63e9c0db62b2afe
Muyizereberissa <b.muyizere@alustudent.com>
2023-11-07 23:56:40 -0800

    9-max_integer.py

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     big_num = my_list[0]
-    for num in (len(my_list)):
+    for num in my_list:
         if num > big_num:
             big_num = num
     return big_num        
