f89a70aa6454851bbe0948accb94a81febe7a1a5
Muyizereberissa <b.muyizere@alustudent.com>
2023-11-08 00:39:29 -0800

    9-max_integer.py

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@ def divisible_by_2(my_list=[]):
     num = 2
     for i in range(len(my_list)):
         x = num*i
-        if x % = 0:
+        if (num*i)% = 0:
             return True
         else:
             return False
