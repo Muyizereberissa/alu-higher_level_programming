edbc859268a092ecc095f2cb7e603c2d23355126
Muyizereberissa <b.muyizere@alustudent.com>
2023-11-08 00:33:18 -0800

    10-divisible_by_2.py

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    num = 2
+    for i in range(len(my_list)):
+        x = num*i
+        if x % = 0:
+            return True
+        else:
+            return False
