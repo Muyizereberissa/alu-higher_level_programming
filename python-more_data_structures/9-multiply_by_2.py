203706509f1479ad395db6762815bef7823b542e
Muyizereberissa <b.muyizere@alustudent.com>
2023-11-09 04:27:19 -0800

    commiting

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    # Create a new dictionary to store the multiplied values
-    multiplied_dict = {}
-
-    # Iterate through the original dictionary
-for key, value in a_dictionary.items():
-        # Multiply each value by 2 and store it in the new dictionary
-        multiplied_dict[key] = value * 2
-
-return multiplied_dict
+      new_dict = dict(a_dictionary)
+    for key, value in new_dict.items():
+        new_dict[key] = value * 2
+    return new_dict
