162b26d267214343a6e34a51d5a39ca03c7e96fb
Muyizereberissa <b.muyizere@alustudent.com>
2023-11-09 02:16:03 -0800

    2-uniq_add.py

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    if not my_list:
-        return ''
-    return list(map(lambda x: {x + number}, my_list))
+    unique_intigers = set()
+    for num in my_list:
+        unique_intiger.add(num)
+    result =  sum(unique_intigers)
+    return result    
