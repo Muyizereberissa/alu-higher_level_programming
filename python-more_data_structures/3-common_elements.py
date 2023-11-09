6e281ffb2c8c26b479b084dd7938b3c057598e19
Muyizereberissa <b.muyizere@alustudent.com>
2023-11-09 03:05:53 -0800

    3-common_elements.py

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    element = {element for element in set_1 if element  in set_2}
+    common_element = set1.intersection(set_2)
+    return common_element
