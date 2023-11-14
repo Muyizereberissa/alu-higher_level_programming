1efc668523d81d98a7bf7494dd782a350a015ef6
Muyizereberissa <b.muyizere@alustudent.com>
2023-11-14 07:30:05 -0800

    1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """this code does not contain a module"""
 class Square:
+
+
     """this is a asquare class that has a size
     __size : a private attribute"""
-
-
     def __init__(self, size):
         """this functions assigns attribute to the objects"""
         self.__size = size
