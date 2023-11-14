3dae3e785dba254855a49fbe41a199e569319fce
Muyizereberissa <b.muyizere@alustudent.com>
2023-11-14 09:10:48 -0800

    2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,18 +4,9 @@ class Square:
     """this is a square class"""
     def __init__(self, size=0):
         """this initializes an atribute to the object"""
-        self.__size = size
-        self.size = size
-      @property
-      def size(self):
-      """getter value for the size attribute"""
-      @size.setter 
-      def size(self, value):
-          """setter method for the attribute size
-           value ; where the value to st must be an integer """
-           if not isinstance(value, int):
-               raise TypeError("size must be an integer")
-           if value < 0:
-               raise ValueError("size must be >= 0")
-
-            self.__size = value       
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self._Square__size = size
