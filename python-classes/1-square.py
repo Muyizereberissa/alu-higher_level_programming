44cc9a4c1385388610d83869824025ecae693a04
Muyizereberissa <b.muyizere@alustudent.com>
2023-11-14 06:43:03 -0800

    1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""this code does not contain a module"""
+class Square:
+    """this is a asquare class"""
+    __size = none
+
+    def __init__(self,size):
+        """this function assigns the attribute to an object"""
+        self.size = size
+square = Square(size)
