60aa3af933a688fd58eb1156d1c2ec306fd62cd2
Muyizereberissa <b.muyizere@alustudent.com>
2023-11-14 07:28:54 -0800

    1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -3,6 +3,8 @@
 class Square:
     """this is a asquare class that has a size
     __size : a private attribute"""
+
+
     def __init__(self, size):
         """this functions assigns attribute to the objects"""
         self.__size = size
