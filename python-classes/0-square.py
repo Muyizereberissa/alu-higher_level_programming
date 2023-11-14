442de992b7d7b5ee061276bcb783c7d5267ab16d
Muyizereberissa <b.muyizere@alustudent.com>
2023-11-14 06:15:36 -0800

    0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 class Square:
-    """ this is a class 'square' that is to be empty """
-    def __init__(self, square):
-        """ this is an initializing function that assigns an atribute to the object""" 
-        self.square = ()
+    """This is a decription of a class"""
+    pass
+square = Square()
