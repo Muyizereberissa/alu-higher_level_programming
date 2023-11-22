b087a971eefdb39703523f4b556e1553f7956515
Muyizereberissa <b.muyizere@alustudent.com>
2023-11-22 10:26:11 -0800

    commits

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Empty BaseGeometry class"""
+
+
+class BaseGeometry:
+    """Class Geometry"""
+
+    def area(self):
+        """Raise Exception """
+        raise Exception('area() is not implemented')
