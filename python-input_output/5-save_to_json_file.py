27dd29355646a26dd72d1b3fb7465b39494ba4c3
Muyizereberissa <b.muyizere@alustudent.com>
2023-11-23 11:56:50 -0800

    5-save_to_json_file.py

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -4,6 +4,17 @@
 
 import json
 
+#!/usr/bin/python3
+"""Save object to a file."""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Save object to a text file using JSON."""
+    with open(filename, 'w+') as f:
+        return json.dump(my_obj, f)
 def save_to_json_file(my_obj, filename):
     """Save object to a text file using JSON."""
     with open(filename, 'w+') as f:
