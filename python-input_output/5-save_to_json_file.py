a0110b1ad22587aa7e08cc0c6171302d5e8fa4d9
Muyizereberissa <b.muyizere@alustudent.com>
2023-11-23 11:55:21 -0800

    5-save_to_json_file.py

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,21 +1,10 @@
 #!/usr/bin/python3
-"""docemented"""
+"""Saving object to a file."""
 
+
+import json
 
 def save_to_json_file(my_obj, filename):
-    """
-    Saves a Python object to a JSON file using the 'with' statement.
-
-    Args:
-        my_obj (object): The object to be saved to the JSON file.
-        filename (str): The path to the JSON file.
-
-    Raises:
-        FileNotFoundError: If the specified file path does not exist.
-    """
-
-    try:
-        with open(filename, 'w+') as f:
-            json.dump(my_obj, f, indent=4)
-    except FileNotFoundError:
-        raise FileNotFoundError(f"Could not find file '{filename}'")
+    """Save object to a text file using JSON."""
+    with open(filename, 'w+') as f:
+        return json.dump(my_obj, f)
