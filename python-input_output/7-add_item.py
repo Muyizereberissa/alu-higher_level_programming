d62a9d0bc6c363e936c8de65d3e1865378bc0dd5
Muyizereberissa <b.muyizere@alustudent.com>
2023-11-23 12:14:50 -0800

    input

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,29 @@
+import sys
+from your_module import save_to_json_file, load_from_json_file
+
+def add_items_to_list_and_save(arguments):
+    """
+    Adds all arguments to a Python list, then saves the list to a JSON file.
+
+    Parameters:
+    - arguments (list): List of arguments to be added to the Python list.
+
+    Returns:
+    - None
+    """
+    # Load existing list from file or create a new one
+    try:
+        existing_list = load_from_json_file('add_item.json')
+    except FileNotFoundError:
+        existing_list = []
+
+    # Add new arguments to the list
+    existing_list.extend(arguments)
+
+    # Save the updated list to the file
+    save_to_json_file(existing_list, 'add_item.json')
+
+if __name__ == "__main__"
+    arguments = sys.argv[1:]
+
+    add_items_to_list_and_save(arguments)
