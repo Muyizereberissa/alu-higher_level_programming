4ffe98a7b99c45d9ba16e7421aff91ac360a614a
Muyizereberissa <b.muyizere@alustudent.com>
2023-11-23 12:08:03 -0800

    input

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,15 @@
+import json
+
+def load_from_json_file(filename):
+    """
+    Creates an object from a JSON file
+
+    Returns:
+    - object: The object created from the JSON file.
+    """
+    with open(filename, 'r') as file:
+        json_data = file.read()
+        # Assuming the JSON file contains a valid JSON string
+        loaded_object = json.loads(json_data)
+
+    return loaded_object
