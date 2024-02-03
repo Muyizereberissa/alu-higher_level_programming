3890bf98dc9348cd4907bb2c6d3264624428ab4c
Muyizereberissa <b.muyizere@alustudent.com>
2024-02-02 23:28:10 -0800

    javascript

---
+++ javascript-warm_up/5-to_integer.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const input = process.argv[2];
+const parsedInput = parseInt(input);
+
+if (!isNaN(parsedInput)) {
+    console.log(`My number: ${parsedInput}`);
+} else {
+    console.log("Not a number");
+}
